30e9612082d800c27a303fca6f18d5796af15252
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-02-03 23:33:13 -0500

    some test code to test interpreter

---
+++ song.py
@@ -0,0 +1,5 @@
+last_verse = '{} bottle of beer on the wall, {} bottle of beer. ' \
+             'take one down, pass it around.. now there are {} ' \
+             'bottles of beer on the wall'
+verse = '{} bottles of beer on the wall, {} bottles of beer. ' \
+        'take one down, pass it around, {} bottles of beer on the wall'

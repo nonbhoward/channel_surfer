c3f4d42928421639eaae0f4b2e754a2281b7f9f5
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-02-27 23:50:19 -0500

    allow logging when testing individual modules

--- cs_networking/__init__.py
+++ cs_networking/__init__.py
@@ -1,4 +1,13 @@
 import logging
+print(f'configuring logger')
+logging.basicConfig(filename='event.log',
+                    filemode='a',
+                    format='%(asctime)s, '
+                           '%(module)s, '
+                           '%(funcName)s, '
+                           '%(lineno)d, '
+                           '%(message)s',
+                    level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 logger.info(f'importing \'{__name__}\'')
 print(f'importing \'{__name__}\'')

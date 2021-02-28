87db6dc1dcefa4b201c1047174f56e3b70ffd34d
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-02-27 23:14:42 -0500

    add step to organize media data

--- main_loop.py
+++ main_loop.py
@@ -7,7 +7,8 @@ logger = logging.getLogger(__name__)
 def main_loop():
     logger.info(f'initializing main loop')
     tv, vcr = Television(), VCR()
-    tv.channels = vcr.fetch_channel_data()
+    channel_data = vcr.fetch_recorded_media()
+    tv.build_programming(channel_data)
     while True:
         logger.info(f'entering main loop')
         tv.play_time_of_day_channel()

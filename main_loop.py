abd667885322752c45f62ecd3bcae6e0f51257fb
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-02-27 20:49:20 -0500

    skeleton

--- main_loop.py
+++ main_loop.py
@@ -4,9 +4,8 @@ from cs_media_library.vcr import VCR
 
 def main_loop():
     tv, vcr = Television(), VCR()
-    tv.play_time_of_day_channel()
     while True:
-        tv.wait_for_event()
+        tv.play_time_of_day_channel()
 
 
 main_loop()

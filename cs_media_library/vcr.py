d3805c35fbfed8532a30037a74f29684d57c153f
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-02-27 23:13:38 -0500

    refactor names

--- cs_media_library/vcr.py
+++ cs_media_library/vcr.py
@@ -10,12 +10,12 @@ class VCR:
         self.supported_file_extensions = ['avi', 'mp4', 'mkv']
         logger.debug(f'supported file extensions : \'{self.supported_file_extensions}\'')
         med_lib_path = get_media_library_path()
-        self.channel_data = get_populated_media_library(med_lib_path)
+        self.recordings = get_populated_media_library(med_lib_path)
 
-    def fetch_channel_data(self):
+    def fetch_recorded_media(self):
         try:
             logger.info(f'fetching channel data')
-            return self.channel_data
+            return self.recordings
         except Exception as e_err:
             print(e_err.args[0])
             logging.error(e_err.args[0])

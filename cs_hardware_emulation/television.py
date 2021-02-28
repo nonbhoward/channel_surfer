2de6cd82374c084d690d3ed3ab218fbbb0262f4a
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-02-27 22:34:14 -0500

    create skeleton

--- cs_hardware_emulation/television.py
+++ cs_hardware_emulation/television.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__)
 
 class Television:
     def __init__(self, minutes_til_shutdown=0):
+        logger.debug(f'initializing \'{self.__class__.__name__}\'')
         """
         :param minutes_til_shutdown: if 0, disabled, else, retrieve a datetime to set as a shutdown threshold
         """
@@ -15,7 +16,7 @@ class Television:
         self.active_channel = self._get_startup_channel()
         self.remote_receiver = Receiver()
         if minutes_til_shutdown:
-            self.auto_shut_down_time = self._get_auto_shutdown_time()
+            self.auto_shut_down_time = self.get_auto_shutdown_time()
 
     def change_channel(self, remote_signal):
         # FIXME what 'event' triggers a change channel call?
@@ -29,14 +30,8 @@ class Television:
         except Exception as e_err:
             print(e_err.args[0])
 
-    def play_time_of_day_channel(self):
-        try:
-            pass
-        except Exception as e_err:
-            print(e_err.args[0])
-
     @staticmethod
-    def _get_auto_shutdown_time() -> datetime:
+    def get_auto_shutdown_time() -> datetime:
         # TODO time_shutdown = time_now - time_to_run
         """
         incomplete function, not implemented
@@ -48,9 +43,17 @@ class Television:
         except Exception as e_err:
             print(e_err.args[0])
 
+    def play_time_of_day_channel(self):
+        try:
+            logging.info(f'playing time of day channel')
+            startup_channel = self._get_startup_channel()
+        except Exception as e_err:
+            print(e_err.args[0])
+
     @staticmethod
     def _get_startup_channel():
         # TODO, restart from last state?
+        logging.info(f'getting startup channel')
         try:
             pass
         except Exception as e_err:

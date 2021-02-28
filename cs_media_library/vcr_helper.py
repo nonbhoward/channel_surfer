6e4b01ac7124eceb6aaa9f14c1d2643f59e51024
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-02-28 00:21:12 -0500

    add docstrings

--- cs_media_library/vcr_helper.py
+++ cs_media_library/vcr_helper.py
@@ -7,55 +7,30 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def get_all_file_paths_in_(media_directory=None, recursive=False) -> list:
+def get_all_file_paths_in_(media_directory=None) -> list:
     """
-    1. TODO, finish func & finish docstring
-    :param media_directory:
-    :param recursive:
-    :return:
+    1. append a constructed Path object for every file in the media directory
+    :param media_directory: media root
+    :return: a list containing Paths
     """
     try:
-        all_files_in_media_directory = list()
+        all_file_paths_in_media_directory = list()
         for root, dirs, files in walk(media_directory):
-            surface_files = list()
-            if not recursive:
-                for _dir in dirs:
-                    surface_files.append(Path(root, _dir).absolute())
             for file in files:
-                all_files_in_media_directory.append(Path(root, file).resolve())
-        return all_files_in_media_directory
+                all_file_paths_in_media_directory.append(Path(root, file).resolve())
+        return all_file_paths_in_media_directory
     except Exception as e_err:
         print(e_err.args[0])
-
-
-def get_all_folders_in_(media_library=None, recursive=False) -> list:
-    """
-    1. if no media library was supplied, raise exception
-    2. begin recursive walk through media library
-    3. if not recursive, return first level of directories found
-    4. else, recursively dive and build list of all directories as Path
-    :param media_library: Path, a location on disk containing media, organized by channel
-    :param recursive: bool, recursively search if True
-    :return: list of Path objects
-    """
-    try:
-        if media_library is None:
-            raise Exception('no media library provided!')
-        all_dirs_recursive = list()
-        for root, dirs, files in walk(media_library):
-            if not recursive:
-                surface_dirs = list()
-                for _dir in dirs:
-                    surface_dirs.append(Path(root, _dir).absolute())
-                return surface_dirs
-            for _dir in dirs:
-                all_dirs_recursive.append(Path(root, _dir))
-        return all_dirs_recursive
-    except Exception as e_err:
-        print(e_err.args[0])
+        logging.error(e_err.args[0])
 
 
 def get_file_extension_from_(file_path) -> str:
+    """
+    1. check if file contains a period, anywhere
+    2. if yes, remove the final delimited text, likely an extension
+    :param file_path: the path to delimit
+    :return: a string that will represent a file extension
+    """
     try:
         logger.debug(f'get file extension from \'{file_path}\'')
         file_extension = ''
@@ -66,9 +41,14 @@ def get_file_extension_from_(file_path) -> str:
         return file_extension
     except Exception as e_err:
         print(e_err.args[0])
+        logging.error(e_err.args[0])
 
 
 def get_media_library_path() -> Path:
+    """
+    1. use this module's location as a basis for finding the defined media root
+    :return: the Path object of the media root
+    """
     try:
         logging.info(f'getting media library path')
         project_path = Path(getcwd())
@@ -83,9 +63,13 @@ def get_media_library_path() -> Path:
 
 def get_populated_media_library(media_lib_path) -> dict:
     """
-    # FIXME entry point for VCR
-    :param media_lib_path:
-    :return:
+    1. recursively get all files in media root
+    2. iterate through all of them, for each..
+    3. ..strip the extension
+    4. ..check that it is supported
+    5. ..add supported files to channel content
+    :param media_lib_path: the media root
+    :return: dict containing media content
     """
     try:
         logging.info(f'populating media library')

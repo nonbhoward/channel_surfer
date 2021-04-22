2e3e82ce06b428e3621d6b6f69d7ca059c5626da
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-04-21 22:31:55 -0400

    refactor: dict to attribute

--- devel/constructor.py
+++ devel/constructor.py
@@ -1,58 +1,113 @@
-from pathlib import Path
-from typing import AnyStr
-from typing import List
+from typing import Tuple
 
 
 class ChannelSection:
-    def __init__(self,
-                 title:                 AnyStr,
-                 start_time_relative,
-                 end_time_relative,
-                 start_time_absolute,
-                 end_time_absolute,
-                 video_file_path:       Path,
-                 video_length,
-                 playlist_index):
+    def __init__(self):
         self._info = {
-            'title':                    title,
-            'start_time_rel':           start_time_relative,
-            'end_time_rel':             end_time_relative,
-            'start_time_abs':           start_time_absolute,
-            'end_time_abs':             end_time_absolute,
-            'video_file_path':          video_file_path,
-            'video_length':             video_length,
-            'playlist_index':           playlist_index
-        }
+            'title':                    None,
+            'start_time_rel':           None,
+            'end_time_rel':             None,
+            'start_time_abs':           None,
+            'end_time_abs':             None,
+            'video_file_path':          None,
+            'video_length':             None,
+            'playlist_index':           None}
 
     @property
-    def info(self):
-        return self._info
+    def end_time_absolute(self):
+        return self._info['end_time_absolute']
 
-    @info.setter
-    def info(self, value):
-        # TODO enforce typing
-        self._info = value
+    @end_time_absolute.setter
+    def end_time_absolute(self, value):
+        self._info['end_time_absolute'] = value
+
+    @property
+    def end_time_relative(self):
+        return self._info['end_time_relative']
+
+    @end_time_relative.setter
+    def end_time_relative(self, value):
+        self._info['end_time_relative'] = value
+
+    @property
+    def playlist_index(self):
+        return self._info['playlist_index']
+
+    @playlist_index.setter
+    def playlist_index(self, value):
+        self._info['playlist_index'] = value
+
+    @property
+    def start_time_absolute(self):
+        return self._info['start_time_absolute']
+
+    @start_time_absolute.setter
+    def start_time_absolute(self, value):
+        self._info['start_time_absolute'] = value
+
+    @property
+    def start_time_relative(self):
+        return self._info['start_time_relative']
+
+    @start_time_relative.setter
+    def start_time_relative(self, value):
+        self._info['start_time_relative'] = value
+
+    @property
+    def title(self):
+        return self._info['title']
+
+    @title.setter
+    def title(self, value):
+        self._info['title'] = value
+
+    @property
+    def video_file_path(self):
+        return self._info['video_file_path']
+
+    @video_file_path.setter
+    def video_file_path(self, value):
+        self._info['video_file_path'] = value
+
+    @property
+    def video_length(self):
+        return self._info['video_length']
+
+    @video_length.setter
+    def video_length(self, value):
+        self._info['video_length'] = value
 
 
 class Channel:
-    def __init__(self,
-                 run_time,
-                 channel_name,
-                 channel_sections:      List[ChannelSection]):
+    def __init__(self):
         self._info = {
-            'run_time':                 run_time,
-            'channel_name':             channel_name,
-            'channel_section_list':     channel_sections
-        }
+            'run_time':                 None,
+            'channel_name':             None,
+            'channel_section_list':     None}
 
     @property
-    def info(self):
-        return self._info
+    def channel_name(self):
+        return self._info['channel_name']
 
-    @info.setter
-    def info(self, value):
-        # TODO enforce typing
-        self._info = value
+    @channel_name.setter
+    def channel_name(self, value):
+        self._info['channel_name'] = value
+
+    @property
+    def channel_section_list(self):
+        return self._info['channel_section_list']
+
+    @channel_section_list.setter
+    def channel_section_list(self, value):
+        self._info['channel_section_list'] = value
+
+    @property
+    def run_time(self):
+        return self._info['run_time']
+
+    @run_time.setter
+    def run_time(self, value):
+        self._info['run_time'] = value
 
 
 class Schedule:
@@ -64,13 +119,13 @@ class Schedule:
         }
 
     @property
-    def info(self):
+    def channels(self):
         return self._info
 
-    @info.setter
-    def info(self, value: tuple):
-        int_key, value = value
-        if isinstance(int_key, int) and isinstance(value, Channel):
-            self._info[int_key] = value
+    @channels.setter
+    def channels(self, value: Tuple[int, Channel]):
+        value_for_channel_integer, value_for_channel = value
+        if isinstance(value_for_channel_integer, int) and isinstance(value_for_channel, Channel):
+            self._info[value_for_channel_integer] = value_for_channel
             return
-        raise TypeError(f'Schedule.info should be a tuple (int, Channel, )')
+        raise TypeError(f'Schedule.channel should be a Tuple[int, Channel]')
